a2febf1fcb43546ddb781915ee371efa9068a1eb
Kris Haamer <kris@mindworks.ee>
2025-09-21 12:42:17 +0300

    Fix: Use Meteor.startup to render Blaze templates

--- client/main.js
+++ client/main.js
@@ -1,6 +1,12 @@
-// Client entry point for Meteor 3.0
+import { Template } from 'meteor/templating';
+import { Blaze } from 'meteor/blaze';
+
 import './templates/elsa.html';
 import './views/elsa_client.js';
+
+Meteor.startup(() => {
+  Blaze.render(Template.layout, document.body);
+});
 
 // Import any other client-side modules here
 // This file serves as the main entry point for all client-side code
